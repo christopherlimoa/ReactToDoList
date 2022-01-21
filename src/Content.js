1f17ef6095fcc8e02163efb3bfd10f919f31ba0c
Christopher Ryan Limoa <clim0061@student.monash.edu>
2022-01-21 13:17:51 +1100

    Add simple list components with props

---
+++ src/Content.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import ItemList from "./ItemList";
+
+const Content = () => {
+	const [items, setItems] = useState([
+		{
+			id: 1,
+			checked: true,
+			item: "Grocery run",
+		},
+		{
+			id: 2,
+			checked: false,
+			item: "Item 2",
+		},
+		{
+			id: 3,
+			checked: false,
+			item: "Item 3",
+		},
+	]);
+
+	const handleCheck = (id) => {
+		const listItems = items.map((item) =>
+			item.id === id ? { ...item, checked: !item.checked } : item
+		);
+		setItems(listItems);
+		localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+	};
+
+	const handleDelete = (id) => {
+		const listItems = items.filter((item) => item.id !== id);
+		setItems(listItems);
+		localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+	};
+
+	return (
+		<main>
+			{items.length ? (
+				<ItemList
+					items={items}
+					handleCheck={handleCheck}
+					handleDelete={handleDelete}
+				/>
+			) : (
+				<p style={{ marginTop: "2rem" }}>Your list is empty.</p>
+			)}
+		</main>
+	);
+};
+
+export default Content;
